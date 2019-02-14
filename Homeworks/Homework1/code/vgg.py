12fb10dbfcf8faded31ac43c4824cd3654a62c25
ruoyaow <ruoyaow@umich.edu>
2019-02-13 20:44:54 -0500

    for debug

--- Homeworks/Homework1/code/vgg.py
+++ Homeworks/Homework1/code/vgg.py
@@ -16,38 +16,71 @@ class VGG(nn.Module):
     # the number of channels and hidden units are decreased compared to the architecture in paper
     def __init__(self):
         super(VGG, self).__init__()
+        # self.conv = nn.Sequential(
+        #     # Stage 1
+        #     # TODO: convolutional layer, input channels 3, output channels 8, filter size 3
+        #     # TODO: max-pooling layer, size 2
+        #     nn.Conv2d(3,8,3),
+        #     nn.MaxPool2d(2),
+        #     # Stage 2
+        #     # TODO: convolutional layer, input channels 8, output channels 16, filter size 3
+        #     # TODO: max-pooling layer, size 2
+        #     nn.Conv2d(8,16,3),
+        #     nn.MaxPool2d(2),
+        #     # Stage 3
+        #     # TODO: convolutional layer, input channels 16, output channels 32, filter size 3
+        #     # TODO: convolutional layer, input channels 32, output channels 32, filter size 3
+        #     # TODO: max-pooling layer, size 2
+        #     nn.Conv2d(16,32,3),
+        #     nn.Conv2d(32,32,3),
+        #     nn.MaxPool2d(2),
+        #     # Stage 4
+        #     # TODO: convolutional layer, input channels 32, output channels 64, filter size 3
+        #     # TODO: convolutional layer, input channels 64, output channels 64, filter size 3
+        #     # TODO: max-pooling layer, size 2
+        #     nn.Conv2d(32,64,3),
+        #     nn.Conv2d(64,64,3),
+        #     nn.MaxPool2d(2),
+        #     # Stage 5
+        #     # TODO: convolutional layer, input channels 64, output channels 64, filter size 3
+        #     # TODO: convolutional layer, input channels 64, output channels 64, filter size 3
+        #     # TODO: max-pooling layer, size 2
+        #     nn.Conv2d(64,64,3),
+        #     nn.Conv2d(64,64,3),
+        #     nn.MaxPool2d(2)
+        # )
         self.conv = nn.Sequential(
             # Stage 1
             # TODO: convolutional layer, input channels 3, output channels 8, filter size 3
             # TODO: max-pooling layer, size 2
             nn.Conv2d(3,8,3),
-            nn.MaxPool2d(2),
-            # Stage 2
-            # TODO: convolutional layer, input channels 8, output channels 16, filter size 3
-            # TODO: max-pooling layer, size 2
-            nn.Conv2d(8,16,3),
-            nn.MaxPool2d(2),
-            # Stage 3
-            # TODO: convolutional layer, input channels 16, output channels 32, filter size 3
-            # TODO: convolutional layer, input channels 32, output channels 32, filter size 3
-            # TODO: max-pooling layer, size 2
-            nn.Conv2d(16,32,3),
-            nn.Conv2d(32,32,3),
-            nn.MaxPool2d(2),
-            # Stage 4
-            # TODO: convolutional layer, input channels 32, output channels 64, filter size 3
-            # TODO: convolutional layer, input channels 64, output channels 64, filter size 3
-            # TODO: max-pooling layer, size 2
-            nn.Conv2d(32,64,3),
-            nn.Conv2d(64,64,3),
-            nn.MaxPool2d(2),
-            # Stage 5
-            # TODO: convolutional layer, input channels 64, output channels 64, filter size 3
-            # TODO: convolutional layer, input channels 64, output channels 64, filter size 3
-            # TODO: max-pooling layer, size 2
-            nn.Conv2d(64,64,3),
-            nn.Conv2d(64,64,3),
             nn.MaxPool2d(2)
+            # # Stage 2
+            # # TODO: convolutional layer, input channels 8, output channels 16, filter size 3
+            # # TODO: max-pooling layer, size 2
+            # nn.Conv2d(8,16,3),
+            # nn.MaxPool2d(2),
+            # # Stage 3
+            # # TODO: convolutional layer, input channels 16, output channels 32, filter size 3
+            # # TODO: convolutional layer, input channels 32, output channels 32, filter size 3
+            # # TODO: max-pooling layer, size 2
+            # nn.Conv2d(16,32,3),
+            # nn.Conv2d(32,32,3),
+            # nn.MaxPool2d(2),
+            # # Stage 4
+            # # TODO: convolutional layer, input channels 32, output channels 64, filter size 3
+            # # TODO: convolutional layer, input channels 64, output channels 64, filter size 3
+            # # TODO: max-pooling layer, size 2
+            # nn.Conv2d(32,64,3),
+            # nn.Conv2d(64,64,3),
+            # nn.MaxPool2d(2),
+            # # Stage 5
+            # # TODO: convolutional layer, input channels 64, output channels 64, filter size 3
+            # # TODO: convolutional layer, input channels 64, output channels 64, filter size 3
+            # # TODO: max-pooling layer, size 2
+            # nn.Conv2d(64,64,3),
+            # nn.Conv2d(64,64,3),
+            # nn.MaxPool2d(2)
         )
         self.fc = nn.Sequential(
             # TODO: fully-connected layer (64->64)
